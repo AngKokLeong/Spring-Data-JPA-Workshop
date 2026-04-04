0fc0210657e5f1d30bb2248a00a09033ecca67a9
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2026-04-04 13:56:52 +0000

    SDJ-005 added Project entity

---
+++ src/main/java/sg/edu/nus/empdemo/entity/Project.java
@@ -0,0 +1,31 @@
+package sg.edu.nus.empdemo.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Projects")
+public class Project {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    @Column(name = "end_date")
+    private LocalDate endDate;
+}
