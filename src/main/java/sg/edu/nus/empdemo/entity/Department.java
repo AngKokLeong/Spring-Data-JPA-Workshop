da15a7a1464cffecbe11249ba9d2797930bdffc4
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2026-04-04 14:04:25 +0000

    SDJ-006 added Department entity

---
+++ src/main/java/sg/edu/nus/empdemo/entity/Department.java
@@ -0,0 +1,21 @@
+package sg.edu.nus.empdemo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Departments")
+public class Department {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+}
