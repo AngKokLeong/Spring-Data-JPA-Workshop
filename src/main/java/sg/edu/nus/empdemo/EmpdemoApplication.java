3bf005657ec4928367a871b97559cef0ea4c5ba2
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2026-04-04 10:58:18 +0000

    SDJ-001 setup Spring Boot Project

---
+++ src/main/java/sg/edu/nus/empdemo/EmpdemoApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.empdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpdemoApplication.class, args);
+	}
+
+}
