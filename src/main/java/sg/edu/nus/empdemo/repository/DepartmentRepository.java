e04ed3d7fca545ac94f35f6f4c0c86d2fd5cddb4
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2026-05-01 16:18:15 +0000

    SDJ-028 added implementation and test for Department repository method to retrieve Department record by department name

--- src/main/java/sg/edu/nus/empdemo/repository/DepartmentRepository.java
+++ src/main/java/sg/edu/nus/empdemo/repository/DepartmentRepository.java
@@ -1,9 +1,13 @@
 package sg.edu.nus.empdemo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import sg.edu.nus.empdemo.entity.Department;
 
+@Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long>{
-    
+    Optional<Department> findByNameEquals(String departmentName);    
 }
--- src/test/java/sg/edu/nus/empdemo/repository/DepartmentRepositoryTest.java
+++ src/test/java/sg/edu/nus/empdemo/repository/DepartmentRepositoryTest.java
@@ -1,19 +1,40 @@
 package sg.edu.nus.empdemo.repository;
 
+import java.util.Optional;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.boot.jpa.test.autoconfigure.TestEntityManager;
+import org.springframework.test.context.TestConstructor;
+
+import sg.edu.nus.empdemo.entity.Department;
 
 @DataJpaTest
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 @DisplayName("Department Repository Test")
 public class DepartmentRepositoryTest {
     
-    private TestEntityManager testEntityManager;
-    private DepartmentRepository departmentRepository;
+    private final TestEntityManager testEntityManager;
+    private final DepartmentRepository departmentRepository;
 
     public DepartmentRepositoryTest(TestEntityManager testEntityManager, DepartmentRepository departmentRepository){
         this.testEntityManager = testEntityManager;
         this.departmentRepository = departmentRepository;
+    }
+
+    @Test
+    @DisplayName("Find Department by exact department name")
+    void findByDepartmentName(){
+
+        Department department = new Department("English");
+
+        this.testEntityManager.persistAndFlush(department);
+        this.testEntityManager.clear();
+
+        Optional<Department> departmentResult = departmentRepository.findByNameEquals(department.getName());
+
+        assertThat(departmentResult.isPresent()).isEqualTo(true);
     }
 
 
