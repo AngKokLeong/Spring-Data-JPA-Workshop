c17247d48e6525039b3ec6955ff638cf522f157b
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2026-05-01 15:43:36 +0000

    SDJ-026 added implementation and test for fetching a project and its assigned employees by Project ID in Project repository

--- src/main/java/sg/edu/nus/empdemo/repository/ProjectRepository.java
+++ src/main/java/sg/edu/nus/empdemo/repository/ProjectRepository.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -20,9 +21,12 @@ public interface ProjectRepository extends JpaRepository<Project, Long>{
 
     List<Project> findByEndDateAfter(LocalDate endDate);
 
-    @Query("SELECT p FROM Project p WHERE p.startDate >= :start AND p.startDate <= :end")
+    @Query("SELECT p FROM Project p WHERE p.startDate >= :start AND p.endDate < :end")
     List<Project> findByDateRange(
         @Param("start") LocalDate startDate, 
         @Param("end") LocalDate endDate);
 
+    @EntityGraph(attributePaths="employees") //EntityGraph allows 
+    @Query("SELECT p FROM Project p WHERE p.id = ?1")
+    List<Project> findByIdWithEmployees(Long id);
 }
